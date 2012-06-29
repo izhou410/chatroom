e1b83b05e9de076d22eb5fa1436c04ca3c1d8d3a
izhou410 <izhou@mit.edu>
2012-06-28 22:24:16 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatroom::Application.config.secret_token = '18ee4169c3b77fb0629b993db0ea130c72159f083a151de63aeb38327f20c211415a80945e480f094baafda1a519ef85453253b5054c767803b6225e8d4b5326'
